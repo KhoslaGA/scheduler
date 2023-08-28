2212def6e82f5804e44ad178d6b22153888ca8bd
Gautam Khosla <gautamkhosla75@gmail.com>
2023-08-28 13:45:53 -0400

    creating DayListItem test file

---
+++ src/components/DayListItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classnames from 'classnames';
+import "components/DayListItem.scss";
+
+
+export default function DayListItem(props) {
+    return (
+        <li onClick={() => props.setDay(props.name)}>
+            <h2 className="text--regular">{props.name}</h2>
+            <h3 className="text--light">{props.spots} spots remaining</h3>
+        </li>
+    );
+}
